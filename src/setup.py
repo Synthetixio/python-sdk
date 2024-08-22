2a360e1dba6d6c0eaa61ff9e06296e44a1a30cc5
Troy <troy.burmeister@gmail.com>
2024-08-22 15:48:17 -0600

    bump version

--- src/setup.py
+++ src/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="synthetix",
-    version="0.1.15",
+    version="0.1.16",
     description="Synthetix protocol SDK",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
